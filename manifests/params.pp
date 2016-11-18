c729a555c2d7fb5185ca09c888d087df2cb1ace2
bjanssens <bjanssens@inuits.eu>
2016-11-18 16:20:02 +0100

    Arch support: Added perl module
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- manifests/params.pp
+++ manifests/params.pp
@@ -597,7 +597,8 @@ class apache::params inherits ::apache::version {
             'fcgid'   => 'mod_fcgid',
             'xml2enc' => 'libxml2', # is an optional dependency in arch
             'php7'    => 'php-apache',
-            'rpaf'    => 'mod_rpaf', # yaourt provides this package
+            'rpaf'    => 'mod_rpaf', # AUR
+            'perl'    => 'mod_perl', # AUR
     }
     $mod_libs         = {
     }
