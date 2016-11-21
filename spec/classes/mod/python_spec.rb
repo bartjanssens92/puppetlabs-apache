d47299afc84ed519931401d2aafd10d9d266020b
bjanssens <bjanssens@inuits.eu>
2016-11-21 17:12:22 +0100

    Arch support: Added python module
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- spec/classes/mod/python_spec.rb
+++ spec/classes/mod/python_spec.rb
@@ -72,4 +72,21 @@ describe 'apache::mod::python', :type => :class do
     it { is_expected.to contain_apache__mod("python") }
     it { is_expected.to contain_package("www-apache/mod_python") }
   end
+  context "on a Archlinux osfamily" do
+    let :facts do
+      {
+        :osfamily               => 'Archlinux',
+        :operatingsystem        => 'Archlinux',
+        :operatingsystemrelease => '4.8.8-1-ARCH',
+        :concat_basedir         => '/dne',
+        :id                     => 'root',
+        :kernel                 => 'Linux',
+        :path                   => '/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl',
+        :is_pe                  => false,
+      }
+    end
+    it { is_expected.to contain_class("apache::params") }
+    it { is_expected.to contain_apache__mod("python") }
+    it { is_expected.to contain_package("mod_python") }
+  end
 end
